abe3baaab0f25159f015319f7b0e7581b1ab5792
TiwariRubi <tiwarirubi93@gmail.com>
2026-09-26 19:05:30 +0530

    Integrate authentication APIs with shared Axios and user context

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -10,9 +10,11 @@ import SignUp from "./pages/Auth/SignUp";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
+import UserProvider from './context/UserContext';
 
 const App = () => {
   return (
+    <UserProvider>
    <div>
      <Router>
        <Routes>
@@ -28,6 +30,7 @@ const App = () => {
      </Router>
 
    </div>
+   </UserProvider>
   );
 };
 
--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,13 @@
-import React ,{ useState } from 'react'
+import React ,{ useContext, useState } from 'react'
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { Link, useNavigate } from 'react-router-dom';
 import Input from "../../components/Inputs/Input";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import uploadImage from '../../utils/uploadImage';
+import { UserContext } from '../../context/UserContext';
 const SignUp = () => {
   const [profilePic, setProfilePic]=useState(null);
   const [fullName, setFullName]=useState("");
@@ -11,6 +15,8 @@ const SignUp = () => {
   const [password, setPassword]=useState("");
   
   const [error,setError]=useState(null);
+
+  const {updateUser} = useContext(UserContext);
 
   const navigate=useNavigate();
 
@@ -34,6 +40,31 @@ const SignUp = () => {
    }
    setError("");
    //signUp API call
+  try{
+    if(profilePic){
+      const imgUploadRes = await uploadImage(profilePic);
+      profileImageUrl = imgUploadRes.imageUrl || "";
+    }
+
+    const response= await axiosInstance.post(API_PATHS.AUTH.REGISTER,{
+      fullName,
+      email,
+      password,
+      profileImageUrl
+    });
+    const { token, user } =response.data;
+    if(token){
+      localStorage.setItem("token", token);
+      updateUser(user);
+      navigate("/dashboard");
+    }
+   }catch(error){
+    if(error.response && error.response.data.message){
+      setError(error.response.data.message);
+    }else{
+      setError("Something went wrong. Please try again");
+    }
+   }
   };
 
   return (
